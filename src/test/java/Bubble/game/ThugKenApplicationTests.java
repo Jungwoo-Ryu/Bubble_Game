65555c5c8759b334375571090c3c72fe353354d5
kenryu <jryu1521@gmail.com>
2024-02-26 11:56:41 +0900

    images added

---
+++ src/test/java/Bubble/game/ThugKenApplicationTests.java
@@ -0,0 +1,13 @@
+package Bubble.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThugKenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
